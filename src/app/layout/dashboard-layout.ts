cf4f159086c4e9d2361d40af643fcc6ec17d7499
ashkan20171 <ashkan.mataee@yahoo.com>
2025-11-27 18:23:53 +0330

    edit ...

--- src/app/layout/dashboard-layout.ts
+++ src/app/layout/dashboard-layout.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { NotificationService } from '../features/notifications/notification.service';
+import { AuthService } from '../core/auth/auth.service';  // اطمینان از وارد کردن AuthService
 
 @Component({
   selector: 'app-dashboard-layout',
@@ -15,20 +16,19 @@ export class DashboardLayout {
   userName = localStorage.getItem('userName') || 'کاربر';
   userRole = localStorage.getItem('role') || 'Employee';
   showNotif: any;
+  currentDirection: 'rtl' | 'ltr' = 'rtl';  // متغیر تعیین جهت متن
 
-  constructor(private router: Router, public notif: NotificationService) {}
-
+  // تغییر سطح دسترسی auth به public
+  constructor(private router: Router, public notif: NotificationService, public auth: AuthService) {}  // تغییر به public
 
   menu = [
-  { title: 'داشبورد', icon: '📊', link: '/dashboard', permission: 'dashboard.view' },
-  { title: 'کاربران', icon: '👥', link: '/users', permission: 'users.view' },
-  { title: 'پروفایل', icon: '🙍‍♂️', link: '/profile', permission: 'profile.view' },
-  { title: 'حضور و غیاب', icon: '⏱️', link: '/attendance', permission: 'attendance.view' },
-  { title: 'حقوق و دستمزد', icon: '💵', link: '/payroll', permission: 'payroll.view' },
-  { title: 'درخواست‌ها', icon: '📝', link: '/requests', permission: 'requests.view' },
-  
-  
-];
+    { title: 'داشبورد', icon: '📊', link: '/dashboard', permission: 'dashboard.view' },
+    { title: 'کاربران', icon: '👥', link: '/users', permission: 'users.view' },
+    { title: 'پروفایل', icon: '🙍‍♂️', link: '/profile', permission: 'profile.view' },
+    { title: 'حضور و غیاب', icon: '⏱️', link: '/attendance', permission: 'attendance.view' },
+    { title: 'حقوق و دستمزد', icon: '💵', link: '/payroll', permission: 'payroll.view' },
+    { title: 'درخواست‌ها', icon: '📝', link: '/requests', permission: 'requests.view' }
+  ];
 
   toggleNotif() {
     this.showNotif = !this.showNotif;
